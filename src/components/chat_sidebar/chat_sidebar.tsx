3aca3974a98a2406c6e96452557f4cbb1184b500
arealclimber <luffulgence@gmail.com>
2024-07-09 17:55:39 +0800

    refactor: 💡 show displayed folder overview navigator icon

--- src/components/chat_sidebar/chat_sidebar.tsx
+++ src/components/chat_sidebar/chat_sidebar.tsx
@@ -461,6 +461,26 @@ const ChatSidebar = ({ getIsExpanded }: IChatSidebarProps) => {
     }
   };
 
+  const displayedFolderOverviewNavigator = isSignedIn && (
+    <Link href={NATIVE_ROUTE.HOME} className="hover:text-button-text-primary">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          className="stroke-current"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M12 2.5V12m0 0l8.5-4.722M12 12L3.5 7.278M12 12v9.5m8.5-4.778l-7.723-4.29c-.284-.158-.425-.237-.575-.268a1.001 1.001 0 00-.403 0c-.15.031-.292.11-.576.268L3.5 16.722M21 16.06V7.942c0-.343 0-.514-.05-.667a1 1 0 00-.215-.364c-.109-.119-.258-.202-.558-.368l-7.4-4.111c-.284-.158-.425-.237-.575-.267a1 1 0 00-.403 0c-.15.03-.292.11-.576.267l-7.4 4.11c-.3.167-.45.25-.558.369a1 1 0 00-.215.364C3 7.428 3 7.599 3 7.942v8.117c0 .342 0 .514.05.666a1 1 0 00.215.364c.109.119.258.202.558.368l7.4 4.111c.284.158.425.237.576.268.133.027.27.027.402 0 .15-.031.292-.11.576-.268l7.4-4.11c.3-.167.45-.25.558-.369a.999.999 0 00.215-.364c.05-.152.05-.324.05-.666z"
+        ></path>
+      </svg>
+    </Link>
+  );
+
   const displayedFolders =
     isSignedIn && // TODO: for demo，實際上要限制沒登入就不能新增資料夾，理論上這個條件不應該發生 (20240628 - Shirley)
     folders &&
@@ -559,25 +579,7 @@ const ChatSidebar = ({ getIsExpanded }: IChatSidebarProps) => {
               </p>
 
               {/* TODO: link to folder list page (20240705 - Shirley) */}
-              {isSignedIn && (
-                <Link href={NATIVE_ROUTE.HOME} className="hover:text-button-text-primary">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      className="stroke-current"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="1.5"
-                      d="M12 2.5V12m0 0l8.5-4.722M12 12L3.5 7.278M12 12v9.5m8.5-4.778l-7.723-4.29c-.284-.158-.425-.237-.575-.268a1.001 1.001 0 00-.403 0c-.15.031-.292.11-.576.268L3.5 16.722M21 16.06V7.942c0-.343 0-.514-.05-.667a1 1 0 00-.215-.364c-.109-.119-.258-.202-.558-.368l-7.4-4.111c-.284-.158-.425-.237-.575-.267a1 1 0 00-.403 0c-.15.03-.292.11-.576.267l-7.4 4.11c-.3.167-.45.25-.558.369a1 1 0 00-.215.364C3 7.428 3 7.599 3 7.942v8.117c0 .342 0 .514.05.666a1 1 0 00.215.364c.109.119.258.202.558.368l7.4 4.111c.284.158.425.237.576.268.133.027.27.027.402 0 .15-.031.292-.11.576-.268l7.4-4.11c.3-.167.45-.25.558-.369a.999.999 0 00.215-.364c.05-.152.05-.324.05-.666z"
-                    ></path>
-                  </svg>
-                </Link>
-              )}
+              {displayedFolderOverviewNavigator}
             </div>
             <div className="hideScrollbar mb-10 mt-5 grow overflow-y-auto overflow-x-hidden">
               {displayedFolders}
