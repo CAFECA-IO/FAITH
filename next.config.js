048951218a0190255c6bcc0c3c73e63178ca9087
RexBearIU <rexbear87941@gmail.com>
2024-09-02 16:01:07 +0800

    feat: add env check

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
+function getEnvVariables(keys) {
+  const missingKeys = [];
+
+  keys.forEach((key) => {
+    const value = process.env[key];
+    if (!value) {
+      missingKeys.push(key);
+    }
+  });
+
+  if (missingKeys.length > 0) {
+    throw new Error(`Missing environment variables: ${missingKeys.join(', ')}`);
+  }
+}
+
+const requiredEnvVars = [
+  'NEXT_PUBLIC_AICH_URL',
+];
+
+// Validate environment variables
+getEnvVariables(requiredEnvVars);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Your Next.js config options
   reactStrictMode: true,
 };
 
